c765890f0a70620093c36022971a75db50167743
IvKr95 <48466057+IvKr95@users.noreply.github.com>
2020-01-10 18:37:34 +0300

    Delete LoginContext.js

--- src/contexts/LoginContext.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const LoginContext = React.createContext({
-  isLoggedIn: false,
-  setIsLoggedIn: () => {},
-});
-LoginContext.displayName = 'LoginContext';
-
-export default LoginContext;
