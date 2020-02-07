9412628235cf0e118662b21a47e46f18b2955b08
Ivan Kravtsov <cold67@mail.ru>
2020-02-07 18:32:07 +0300

    Create AddContacts.jsx

---
+++ src/client/forms/AddContacts.jsx
@@ -0,0 +1,51 @@
+/* eslint-disable linebreak-style */
+import React, { useState } from 'react';
+import styles from '../../admin/css/admin.module.css';
+
+const DEFAULT_EMAIL = '';
+
+// Форма добавления контактов,
+// на которые нужно будет отправить
+// сообщение о заказе билета
+const AddContacts = (props) => {
+  const {
+    onAddEmail: addEmail,
+  } = props;
+
+  const [email, setEmail] = useState(DEFAULT_EMAIL);
+
+  const handleChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    addEmail(email);
+    setEmail(DEFAULT_EMAIL);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label className={`${styles['conf-step__label']} ${styles['conf-step__label-fullsize']}`} htmlFor="email">
+        <h1>Введите адресс электронной почты для получения билета</h1>
+        <input
+          id="email"
+          className={styles['conf-step__input']}
+          type="email"
+          placeholder="Электронная почта"
+          name="email"
+          value={email}
+          onChange={handleChange}
+          required
+        />
+      </label>
+      <div className={`${styles['conf-step__buttons']} ${styles['text-center']}`}>
+        <button type="submit" className={`${styles['conf-step__button']} ${styles['conf-step__button-regular']}`}>
+          Отправить
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default AddContacts;
