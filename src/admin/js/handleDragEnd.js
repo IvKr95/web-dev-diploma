f19ed824ba81c89c131447f643ccbc0b23707eb9
Ivan Kravtsov <cold67@mail.ru>
2020-02-07 18:32:30 +0300

    Delete handleDragEnd.js

--- src/admin/js/handleDragEnd.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable linebreak-style */
-const handleDragEnd = (e) => {
-  e.target.classList.remove('hold');
-  e.target.classList.remove('invisible');
-};
-
-export default handleDragEnd;
